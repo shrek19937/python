2b5ae872916a783c7e4fcbf0e9d6ac5834cd5a06
shrek19937 <qing.wu@gmail.com>
2022-04-10 14:45:34 -0500

    added collection data types - list, tuple, set, dictionary

--- exe/collections.py
+++ exe/collections.py
@@ -1,19 +1,13 @@
+#python class
+#file handling
+#exception handling
 #list of python collection class
 #basic ops, CRUD
 #list comprehension
-#classes
-#file handling
-#exception handling
 #re
-
 from pathlib import Path
 
-list1 = [1, 2, 3]
-list2 = [3, 4, 5]
-
-#list comprehension
-common = [ i for i in list1 if i in list2 ]; print(common)   #find common list
-
+#python class
 class auser:
     def __init__(self, id, fname, lname):
         self.id    = id
@@ -26,11 +20,34 @@ class auser:
 auser = auser(1, "michael", "wu")
 print(auser)
 
-def read_file(fname):
+#file handling
+def print_file(fname):
     with open(fname, "r") as f:
         for line in f:
             print(f"{line}", end = "")
-read_file(f"{str(Path.home())}/python-code/exe/read-stdin.py")
+    print()
+print_file(f"{str(Path.home())}/python-code/exe/read-stdin.py")
+
+#collection data types
+#  list, tuple, set, dictionary. stack could be implemented using list, see below
+#basic ops, CRUD
+list1 = [1, 2, 3]
+list2 = [3, 4, 5]
+aset  = set()
+aset.add(1); aset.add(5), aset.add("A")
+adict = {"a":1, "b":2, "c":3}
+adict.update({"d":4})  #dict add/update
+adict["e"] = 5         #dict add
+print(aset)
+print(adict)
+atuple = ("fname", "shrek")
+print(atuple)
+
+#list comprehension
+common  = [ i for i in list1 if i in list2 ]; print(common)   #find common list
+common2 = [ a for a in list2 if a in aset ];  print(common2)
+#collection - stack implemented using list
+#exception handling
 
 #stack to validate match parameters
 openers = { '{':'}', '[':']', '(':')' }
